54209ba5d64b5c07f89d6e8afc7d5e0e5b2cedd3
SeungHyeon99 <146049032+bsyjsh88@users.noreply.github.com>
2024-06-18 20:58:53 +0900

    2024-06-18

---
+++ css/shop4/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.navbar_toggleBtn').on('click', () =>{
+        // 클래스 선택자 생성
+        $('.navbar_menu').toggle("active");
+    });
+    $(window).resize(() =>{
+        // nav의 너비값이 문자이므로 숫자로 형변환함
+        if(parseInt($('nav').css('width')) > 768){
+            // 보이기- display: flex, 숨기기 - display: none
+            $('.navbar_menu').css('display', 'flex');
+        }
+    })
+})
